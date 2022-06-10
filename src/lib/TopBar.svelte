663a7fa23fc9cbdb58c8dfd361ebc1fd02779e75
neonfuz <neonfuz@gmail.com>
2022-06-09 21:29:28 -0500

    TopBar: initial commit

---
+++ src/lib/TopBar.svelte
@@ -0,0 +1,36 @@
+<script>
+ const items = [
+     "Home",
+     "About",
+     "Planning",
+     "AAII Journal",
+     "Markets",
+     "Model Portfolios",
+     "Investing Ideas",
+     "Screening",
+     "Community",
+     "Education",
+     "A+ Investor",
+ ];
+</script>
+
+<nav class="TopBar">
+    {#each items as item}
+        <a href="">{item}</a>
+    {/each}
+</nav>
+
+<style>
+ .TopBar {
+     color: white;
+     background: #10283d;
+     display: flex;
+     justify-content: center;
+     font-weight: bold;
+ }
+ .TopBar a {
+     color: white;
+     text-decoration: none;
+     padding: 1em .6em;
+ }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
 <script>
+import TopBar from '$lib/TopBar.svelte';
  import StockTable from '$lib/StockTable.svelte';
 </script>
 
+<TopBar />
 <StockTable />
